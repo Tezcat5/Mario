f1695211e2d50d84553d1a9c701a859b76e1342e
Tezcat5 <vicpero96@gmail.com>
2024-01-17 10:39:20 +0100

    Jump

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody2D rBody;
+    public GroundSensor sensor;
+
     public Vector3 newPosition = new Vector3 (58, 5, 0);
 
     public float movementSpeed = 5;
     private float inputhorizontal;
-
+    public float jumpForce = 10;
     public bool jump = false;
 
+    void Awake ()
+{
+
+    rBody = GetComponent<Rigidbody2D>();
+}
     // Start is called before the first frame update
     void Start()
     {
@@ -24,21 +32,35 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         inputhorizontal = Input.GetAxis("Horizontal");
-        //transform.position = transform.position + new Vector3 (1, 0, 0) * movementSpeed * Time.deltaTime;
-        transform.position += new Vector3 (inputhorizontal, 0, 0) * movementSpeed * Time.deltaTime;
 
-        if(jump == true);
-        
+        //transform.position = transform.position + new Vector3 (1, 0, 0) * movementSpeed * Time.deltaTime;
+        //transform.position += new Vector3(inputhorizontal, 0, 0) * movementSpeed * Time.deltaTime;
+
+        /*if(jump == true)
         {
             Debug.Log("estoy saltando");
         }
-            else if(jump == false);
+        else if(jump == false)
         {
-            Debug.Log("Estoy en el suelo");
+            Debug.Log("estoy en el suelo");
         }
-       else
+        else
         {
-            Debug.Log;
+            Debug.Log("estoy saltando");
+        }*/
+
+        if(Input.GetButtonDown("Jump") && sensor.isGrounded == true)
+        {
+            if(sensor.isGrounded == true)
+            {
+                rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
         }
     }
+
+    void FixedUpdate()
+    {
+        rBody.velocity = new Vector2(inputhorizontal * movementSpeed, rBody.velocity.y);
+    }
+ 
 }
